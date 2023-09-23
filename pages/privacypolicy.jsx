b481bd7a45dcd52a99765952e60797a15fa5f73d
franespino <frapo_developer@outlook.com.pe>
2023-09-23 11:39:39 -0500

    policy security

---
+++ pages/privacypolicy.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import styles from '../styles/Home.module.css';
+
+const privacypolicy = () => {
+  return (
+  
+    <div className={styles.ContextTerms} >
+    <h1>Términos y Condiciones de Uso de la Aplicación "Guardar Lugares"</h1>
+    <p><strong>Fecha de vigencia:</strong> Septiembre de 2030</p>
+
+    <p>
+      Por favor, lea detenidamente los siguientes términos y condiciones de uso (en adelante, "Términos") antes de utilizar la aplicación "Guardar Lugares" (en adelante, la "Aplicación"). Estos Términos establecen los derechos y obligaciones tanto del usuario de la Aplicación ("Usuario") como del desarrollador de la Aplicación ("Desarrollador").
+    </p>
+
+    <h2>1. Aceptación de los Términos</h2>
+    <p>
+      Al descargar, instalar o utilizar la Aplicación "Guardar Lugares", el Usuario acepta los siguientes Términos y se compromete a cumplirlos en su totalidad. Si no está de acuerdo con estos Términos, no utilice la Aplicación.
+    </p>
+
+    <h2>2. Uso de la Ubicación del Usuario</h2>
+    <p>
+      La Aplicación "Guardar Lugares" utiliza la ubicación del Usuario para permitirle guardar y gestionar lugares de interés. El Usuario otorga su consentimiento para que la Aplicación acceda a su ubicación geográfica. La información de ubicación se utilizará únicamente con el propósito de proporcionar servicios relacionados con la Aplicación y no será compartida con terceros sin el consentimiento expreso del Usuario.
+    </p>
+
+    <h2>3. Responsabilidad del Usuario</h2>
+    <p>
+      El Usuario es el único responsable de la información que almacene en la Aplicación y de su uso. El Usuario acepta no utilizar la Aplicación para actividades ilegales o inapropiadas y se compromete a cumplir todas las leyes y regulaciones aplicables.
+    </p>
+
+    <h2>4. Privacidad</h2>
+    <p>
+      La política de privacidad de la Aplicación "Guardar Lugares" se encuentra disponible en [URL de la política de privacidad]. El Usuario debe revisar y aceptar la política de privacidad de la Aplicación antes de utilizarla.
+    </p>
+
+    <h2>5. Actualización y Modificaciones</h2>
+    <p>
+      El Desarrollador se reserva el derecho de actualizar o modificar estos Términos en cualquier momento. Se notificará al Usuario de cualquier cambio importante a través de la Aplicación. El uso continuado de la Aplicación después de cualquier modificación constituirá la aceptación de los nuevos Términos.
+    </p>
+
+    <h2>6. Cancelación y Terminación</h2>
+    <p>
+      El Usuario puede cancelar su cuenta en la Aplicación en cualquier momento. El Desarrollador puede, a su discreción, terminar o suspender la cuenta del Usuario en caso de violación de estos Términos o por cualquier otra razón válida.
+    </p>
+
+    <h2>7. Limitación de Responsabilidad</h2>
+    <p>
+      El Desarrollador no será responsable de ningún daño directo o indirecto causado por el uso de la Aplicación. La Aplicación se proporciona "tal cual" y el Usuario la utiliza bajo su propio riesgo.
+    </p>
+
+    <h2>8. Contacto</h2>
+    <p>
+      Para cualquier pregunta, comentarios o preocupaciones relacionados con la Aplicación "Guardar Lugares" o estos Términos, por favor, póngase en contacto con el Desarrollador en el correo electrónico: <a href="mailto:fraporitmos@gmail.com">fraporitmos@gmail.com</a>.
+    </p>
+
+    <p>
+      Al utilizar la Aplicación "Guardar Lugares", el Usuario reconoce haber leído y comprendido estos Términos y acepta estar legalmente obligado por ellos. Estos Términos constituyen un acuerdo legal entre el Usuario y el Desarrollador.
+    </p>
+
+    <p><strong>Nombre del Desarrollador:</strong> Fraporitmos</p>
+    <p><strong>Correo Electrónico del Desarrollador:</strong> fraporitmos@gmail.com</p>
+  </div>
+  );
+};
+
+export default privacypolicy;
